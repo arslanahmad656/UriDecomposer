fbae7b91bfff4109dd273ed161bef01d0ff7b424
Arslan Ahmad <ars.ahm6@gmail.com>
2019-08-05 17:02:16 +0500

    Completed

--- UriDecomposer/Form1.Designer.cs
+++ UriDecomposer/Form1.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TxtQueryBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnDecompse = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TxtQueryBox
+            // 
+            this.TxtQueryBox.Location = new System.Drawing.Point(111, 28);
+            this.TxtQueryBox.Name = "TxtQueryBox";
+            this.TxtQueryBox.Size = new System.Drawing.Size(526, 20);
+            this.TxtQueryBox.TabIndex = 0;
+            this.TxtQueryBox.Enter += new System.EventHandler(this.TxtQueryBox_Enter);
+            this.TxtQueryBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtQueryBox_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Url or Query String:";
+            // 
+            // BtnDecompse
+            // 
+            this.BtnDecompse.Location = new System.Drawing.Point(643, 25);
+            this.BtnDecompse.Name = "BtnDecompse";
+            this.BtnDecompse.Size = new System.Drawing.Size(75, 23);
+            this.BtnDecompse.TabIndex = 2;
+            this.BtnDecompse.Text = "Decompose";
+            this.BtnDecompse.UseVisualStyleBackColor = true;
+            this.BtnDecompse.Click += new System.EventHandler(this.BtnDecompse_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(15, 68);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.ReadOnly = true;
+            this.TxtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.TxtResult.Size = new System.Drawing.Size(703, 267);
+            this.TxtResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(730, 347);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnDecompse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtQueryBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "URI Decomposer";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TxtQueryBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnDecompse;
+        private System.Windows.Forms.TextBox TxtResult;
     }
 }
 
--- UriDecomposer/Form1.cs
+++ UriDecomposer/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Imanami.TroubleshootingUtility.Common;
 
 namespace UriDecomposer
 {
@@ -15,6 +16,46 @@ namespace UriDecomposer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnDecompse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(TxtQueryBox.Text))
+                {
+                    throw new Exception("No query entered in query box.");
+                }
+
+                var query = TxtQueryBox.Text;
+                var parameters = Utility.DecomposeQueryParameters(query, true);
+                TxtResult.Clear();
+                var sb = new StringBuilder();
+                parameters.Select(kv => $"{kv.Key}: {kv.Value}").ToList().ForEach(p => sb.AppendLine(p));
+                TxtResult.Text = sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            BtnDecompse.PerformClick();
+        }
+
+        private void TxtQueryBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                BtnDecompse.PerformClick();
+            }
+        }
+
+        private void TxtQueryBox_Enter(object sender, EventArgs e)
+        {
+            (sender as TextBox).SelectAll();
         }
     }
 }
